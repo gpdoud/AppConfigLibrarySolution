2c372062606281f67c2d83854599c8aadb7e97cb
Greg Doud <gpdoud@gmail.com>
2019-10-06 23:04:11 -0400

    Add project files.

---
+++ AppConfigJsonLibrary/AppConfigJson.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
+namespace AppConfigJsonLibrary {
+
+    public class AppConfigJson<T> : IDisposable {
+
+        string jsonfile = default(string);
+        public T AppConfig = default(T);
+
+        private void Serialize(T AppConfig, string jsonfile) {
+            var options = new JsonSerializerOptions() { WriteIndented = true };
+            var json = JsonSerializer.Serialize<T>(AppConfig, options);
+            System.IO.File.WriteAllText(jsonfile, json);
+        }
+        private void Deserialize(T AppConfig, string jsonfile) {
+            var json = System.IO.File.ReadAllText(jsonfile);
+            this.AppConfig = JsonSerializer.Deserialize<T>(json);
+        }
+        public AppConfigJson(string jsonfile) {
+            this.jsonfile = jsonfile;
+            Deserialize(this.AppConfig, jsonfile);
+        }
+        public void Dispose() {
+            Serialize(this.AppConfig, this.jsonfile);
+        }
+    }
+}
---
+++ TestConsole/Program.cs
@@ -0,0 +1,34 @@
+﻿using AppConfigJsonLibrary;
+using System;
+
+namespace TestConsole {
+
+    class Program {
+
+        static void Main(string[] args) {
+            var jsonfile = @"C:\repos\dotnetcore30\AppConfigLibrarySolution\TestConsole\appConfig.json";
+            using(var cfg = new AppConfigJson<App.Config>(jsonfile)) {
+                cfg.AppConfig.Name = "Greg";
+                cfg.AppConfig.Sql = new App.Sql();
+                cfg.AppConfig.Sql.Server = @"localhost\sqlexpress";
+                cfg.AppConfig.Sql.Database = "AppDb";
+                cfg.AppConfig.Sql.TrustedConnection = true;
+            }
+        }
+    }
+}
+
+namespace App {
+    public class Sql {
+        public string Server { get; set; } = @"server\sqlexpress";
+        public string Database { get; set; } = "AppDb";
+        public bool TrustedConnection { get; set; } = true;
+    }
+    public class Config {
+        public string Name { get; set; }
+        public int Counter { get; set; } = 1;
+        public bool Active { get; set; } = true;
+        public DateTime DOB { get; set; } = new DateTime(1957, 8, 27);
+        public Sql Sql { get; set; } = new Sql();
+    }
+}
